17042b92ffa9fa4bff7776d61ec364c6f6e62460
Udip Parajuli <udipparajuli17@gmail.com>
2021-07-08 09:03:28 +0545

    Converted the machine timestamp to en-US timestamp

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,20 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", "2-digit");
+  const year = props.date.getFullYear();
+
   return (
     <div className="expense-item">
-      <div>Date</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
-        <h2>Title</h2>
-        <div className="expense-item__price">Cost</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
