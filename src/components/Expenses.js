72208c1f0e6b67f74509a4e0ab67aae4f56e9863
Udip Parajuli <udipparajuli17@gmail.com>
2021-07-08 14:45:44 +0545

    Created Expense components

---
+++ src/components/Expenses.js
@@ -0,0 +1,14 @@
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+
+function Expenses(props) {
+  return (
+    <div className="expenses">
+      <ExpenseItem date={props.items[0].date} title={props.items[0].title} amount={props.items[0].amount}></ExpenseItem>
+      <ExpenseItem date={props.items[1].date} title={props.items[1].title} amount={props.items[1].amount}></ExpenseItem>
+      <ExpenseItem date={props.items[2].date} title={props.items[2].title} amount={props.items[2].amount}></ExpenseItem>
+    </div>
+  );
+}
+
+export default Expenses;
