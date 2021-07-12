9de94d4b568ab314732d40d5934bf3325cf8cde3
Udip Parajuli <udipparajuli17@gmail.com>
2021-07-12 21:24:13 +0545

    using props children

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,12 +1,13 @@
+import Card from "./Card";
 import ExpenseItem from "./ExpenseItem";
 
 function Expenses(props) {
   return (
-    <div>
+    <Card className="expenses">
       <ExpenseItem date={props.items[0].date} title={props.items[0].title} amount={props.items[0].amount}></ExpenseItem>
       <ExpenseItem date={props.items[1].date} title={props.items[1].title} amount={props.items[1].amount}></ExpenseItem>
       <ExpenseItem date={props.items[2].date} title={props.items[2].title} amount={props.items[2].amount}></ExpenseItem>
-    </div>
+    </Card>
   );
 }
 
